3bf4bf6c0059e60c7ac0505cc6a354c189ab28dc
Ali Parlakçı <parlakciali@gmail.com>
2021-04-20 19:47:21 +0300

    setup.py: forbid installing below 3.9

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # encoding=utf-8
 
+import sys
+
 from setuptools import setup
 
-setup(setup_requires=['pbr', 'appdirs'], pbr=True, data_files=[('config', ['bdfr/default_config.cfg'])])
+if sys.version_info >= (3,9):
+    setup(setup_requires=['pbr', 'appdirs'], pbr=True, data_files=[('config', ['bdfr/default_config.cfg'])])
+else:
+    raise RuntimeError("This package requres Python 3.9+")
